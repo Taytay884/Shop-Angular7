827018f630a8d72796a2bdd760ee4a64f19dca80
Itay Ben Shmuel <Taytay884@yahoo.com>
2018-11-21 17:19:02 +0200

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeLayoutComponent} from './home/layout/layout.component';
+import {ShopModule} from './shop/shop.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeLayoutComponent, pathMatch: 'full'},
+  {path: 'shop', loadChildren: () => ShopModule}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
+import { HomeModule } from './home/home.module';
+import { ShopModule } from './shop/shop.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UiModule,
+    HomeModule,
+    ShopModule
   ],
   providers: [],
   bootstrap: [AppComponent]
